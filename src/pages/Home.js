e031023af436868503ffddf6cd8f2c69ed2ad18e
EdmundJohnCruz <edmundjohncruz@gmail.com>
2020-12-22 23:58:56 -0800

    Using bootstrap to implement sticky navbar on home page.
    Started using react bootstrap; created a "components" folder which has the navigation bar in it so far.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,16 @@
 import React from 'react';
+/*Page/Component Imports*/
 import '../css/Home.css';
+import TopNavBar from '../pages/components/TopNavBar';
 
 const Home = () => {
-    return (
-        <div className="Home">
-          <header className="Home-header">
-            <p>Here's the homepage.</p>
-          </header>
-        </div>
-      );
+  return (
+    <div className="Home">
+      <TopNavBar />
+      <header className="Home-header">
+        <p>Here's the homepage.</p>
+      </header>
+    </div>
+  );
 }
 export default Home;
---
+++ src/pages/components/TopNavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+/* React Bootstrap Imports*/
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const TopNavBar = () => {
+    return (
+        <Navbar bg="dark" variant="dark" sticky="top">
+            <Navbar.Brand href="/home">Almaga</Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link>Pages</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/">Landing</Nav.Link>
+            </Nav>
+            <Form inline>
+                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                <Button variant="outline-info">Search</Button>
+            </Form>
+        </Navbar>
+    )
+}
+export default TopNavBar;
